6a55e80c33bb628925b226d7fc7ea20356713722
João Afonso Katombela <ja3328173@gmail.com>
2024-03-26 14:50:39 +0100

    fix monitor

--- src/controllers/monitoring.brand.ddg.controller.ts
+++ src/controllers/monitoring.brand.ddg.controller.ts
@@ -16,20 +16,19 @@ function getSentiment(snippet: string): string {
   }
 }
 
-export async function MonitorBrandMentions(req: Request, res: Response) {
-  const { query, region } = req.body;
+export async function getDDGMentions(req: Request, res: Response) {
+  const { query } = req.body;
 
   const options = {
     method: "GET",
     url: "https://real-time-web-search.p.rapidapi.com/search",
     params: {
       q:
-      "intitle:" +
-      query +
-      " intext:" +
-      query +
-        " site:youtube.com OR site:instagram.com OR site:facebook.com OR site:linkedin.com OR site:twitter.com OR site:pinterest.com OR site:tumblr.com OR site:reddit.com OR site:snapchat.com OR site:whatsapp.com OR site:telegram.org OR site:tiktok.com OR site:weibo.com OR site:vine.co OR site:periscope.tv OR site:forumotion.com OR site:disqus.com OR site:reddit.com OR site:stackoverflow.com OR site:blogspot.com OR site:medium.com OR site:wordpress.com OR site:angola24horas.com OR site:jornaldeangola.ao OR site:opais.co.ao OR site:angop.ao OR site:sapo.pt OR site:globo.com OR site:estadao.com.br OR site:folha.uol.com.br OR site:g1.globo.com " + region,
-      limit: "50",
+       
+        
+        query +
+        " site:youtube.com OR site:instagram.com OR site:facebook.com OR site:linkedin.com OR site:twitter.com OR site:pinterest.com OR site:tumblr.com OR site:reddit.com OR site:snapchat.com OR site:whatsapp.com OR site:telegram.org OR site:tiktok.com OR site:weibo.com OR site:vine.co OR site:periscope.tv OR site:forumotion.com OR site:disqus.com OR site:reddit.com OR site:stackoverflow.com OR site:blogspot.com OR site:medium.com OR site:wordpress.com OR site:angola24horas.com OR site:jornaldeangola.ao OR site:opais.co.ao OR site:angop.ao OR site:sapo.pt OR site:globo.com OR site:estadao.com.br OR site:folha.uol.com.br OR site:g1.globo.com",
+      limit: "100",
     },
 
     headers: {
