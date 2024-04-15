7f2ffc51e99918db4e4642103a903efeae1e081b
João Afonso Katombela <ja3328173@gmail.com>
2024-04-15 13:02:02 +0100

    add wpp api

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
-// index.js
-const express = require("express")
-const messageRouter = require('./routers/messageRouter')
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors"); // Importe o middleware cors
 const { startWhatsAppConnection, getQRCode } = require('./services/WhatsappClient');
 
-const app = express()
-app.use(express.json())
-app.use(messageRouter)
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Habilitar o CORSapp.use(cors({ origin: '*' }));
+
+app.use(cors({ origin: '*' }));
+
 
 app.get('/', async (req, res) => {
   try {
-    // Aqui você deve realizar a autenticação do usuário com o Firebase
-    // Suponha que o usuário já está autenticado com sucesso
     // Inicie a conexão com o WhatsApp
     startWhatsAppConnection();
     // Obtenha o QR code
     const qrCode = await getQRCode();
-    // Retorne o QR code para o cliente
-    res.status(200).json({ qrCode });
+    // Envie o QR code para o cliente através do WebSocket
+    io.emit('qrCode', qrCode);
+    res.status(200).send(qrCode);
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal server error');
   }
 });
 
-app.listen(3001, () => console.log(`Server is ready in on port 3001`))
+server.listen(3001, () => console.log(`Server is ready on port 3001`));
--- src/services/WhatsappClient.js
+++ src/services/WhatsappClient.js
@@ -1,6 +1,8 @@
 // whatsappClient.js
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
+const path = require('path');
+
 
 const whatsappclient = new Client({
   authStrategy: new LocalAuth(),
@@ -39,4 +41,28 @@ const getQRCode = () => {
   });
 }
 
-module.exports = { startWhatsAppConnection, getQRCode };
+
+// Pasta onde as sessões serão armazenadas
+const sessionDir = path.join(__dirname, '..', 'sessions');
+
+// Função para obter a sessão de um usuário
+function getSession(userId) {
+    const sessionFilePath = path.join(sessionDir, `${userId}.json`);
+    try {
+        // Tenta ler o arquivo de sessão do usuário
+        const sessionData = fs.readFileSync(sessionFilePath, 'utf8');
+        return JSON.parse(sessionData);
+    } catch (error) {
+        // Se o arquivo não existe ou ocorrer outro erro, retorna null
+        return null;
+    }
+}
+
+// Função para salvar a sessão de um usuário
+function saveSession(userId, sessionData) {
+    const sessionFilePath = path.join(sessionDir, `${userId}.json`);
+    fs.writeFileSync(sessionFilePath, JSON.stringify(sessionData));
+}
+
+
+module.exports = { startWhatsAppConnection, getSession, saveSession, getQRCode };
